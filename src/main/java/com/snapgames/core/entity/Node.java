d151b8194dfa5af8480b5b39b22afa82b9450814
Frédéric Delorme <frederic.delorme@gmail.com>
2023-09-17 16:04:49 +0200

    feat(Node): Add the Node class and adapt Entity

---
+++ src/main/java/com/snapgames/core/entity/Node.java
@@ -0,0 +1,43 @@
+package com.snapgames.core.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+    private static int index = 0;
+    int id = index++;
+
+    public boolean active;
+    private String name = "node_" + id;
+
+    private Node parent;
+
+    private List<Node> children = new ArrayList<>();
+
+    protected Node() {
+        this.active = true;
+    }
+
+    protected Node(String name) {
+        this();
+        this.name = name;
+    }
+
+    public void addChild(Node child){
+        children.add(child);
+        child.setParent(this);
+    }
+
+    public void setParent(Node node) {
+        this.parent = parent;
+    }
+
+    public static int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
